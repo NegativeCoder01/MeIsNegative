4e740e9d93ec9b5015ad5b55351ab6738408d388
Negative <negativestudiomain@gmail.com>
2025-05-18 21:46:20 +0000

    hehe

--- Csharp/NumbersAndMath/NumberCombinations/obj/Debug/net9.0/NumberCombinations.AssemblyInfo.cs
+++ Csharp/NumbersAndMath/NumberCombinations/obj/Debug/net9.0/NumberCombinations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberCombinations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0bafb3a3f189759d07895d92fc53fbbfa8e5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6547576c462f192caa2aec40073731878159412")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberCombinations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberCombinations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Python/messinaround/characters/project.py
@@ -0,0 +1,2 @@
+for code_point in range(0x0, 0x10FFFF):  # Range for all Unicode characters
+    print(chr(code_point), end=' ')
